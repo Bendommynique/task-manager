9204b5c898d485a532f9eb7b3a517ca0713d5e35
Dominic Njokuocha <dommyn4u@gmail.com>
2022-11-07 14:56:54 +0100

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './App.css';
+import { useState } from 'react';
+import NewTask from "../NewTask/NewTask";
+import TasksList from "../TasksList/TasksList";
+
+export default function App() {
+    const [newTask, setNewTask] = useState({});
+    const handleChange = ({ target }) => {
+      const { name, value } = target;
+      setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+    };
+  
+    const [allTasks, setAllTasks] = useState([]);
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (!newTask.title) return;
+      setAllTasks((prev) => [newTask, ...prev]);
+      setNewTask({});
+    };
+    const handleDelete = (taskIdToRemove) => {
+      setAllTasks((prev) => prev.filter(
+        (task) => task.id !== taskIdToRemove
+      ));
+    }; 
+  
+    return (
+      <main>
+        <h1>Tasks</h1>
+        <NewTask
+          newTask={newTask}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+        <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+      </main>
+    );
+  }
